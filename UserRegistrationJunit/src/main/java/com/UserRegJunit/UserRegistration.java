07edc531366214a3d4519a2b528fa5b938771dc2
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-10-02 13:27:59 +0530

    TEST:All valide email

--- UserRegistrationJunit/src/main/java/com/UserRegJunit/UserRegistration.java
+++ UserRegistrationJunit/src/main/java/com/UserRegJunit/UserRegistration.java
@@ -3,34 +3,10 @@ package com.UserRegJunit;
 import java.util.regex.*;
 
 public class UserRegistration {
-	public boolean checkValidFirstName(String firstName)
-	{
-		String firstNameRegex  = "^[A-Z]{1}[a-z]{2,}$";
-	    Pattern pattern = Pattern.compile(firstNameRegex );
-	    Matcher matcher = pattern.matcher(firstName);
-	    return matcher.matches();
-
-	}
-	public boolean checkValidLastName(String lastName)
-	{
-        String lastNameRegex = "^[A-Z]{1}[a-z]{2,}$";
-        Pattern pattern = Pattern.compile(lastNameRegex);
-        Matcher matcher = pattern.matcher(lastName);
-        return matcher.matches();
-    }
-	public boolean checkValidMobileNumber(String mobileNumber)
-	{
-        String mobileRegex = "^(91){1}[ ]+[0-9]{10}$";
-        Pattern patternObject = Pattern.compile(mobileRegex);
-        Matcher matcher = patternObject.matcher(mobileNumber);
-        return matcher.matches();
-     }	
+			
 	public boolean checkValidEmail(String email)
 	{
-
-        boolean isEmail;
-        String emailRegex = ("[a-z0-9@.]*");
-        Pattern patternObject = Pattern.compile(emailRegex);
+        Pattern patternObject = Pattern.compile("^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$");
         Matcher matcher = patternObject.matcher(email);
         return matcher.matches();
 
@@ -38,12 +14,7 @@ public class UserRegistration {
 	
 
 
-	public boolean password(String password) {
-        String regex ="^(?=.*[A-Z]){1}(?=.*[a-z])(?=.*[0-9]){1}(?=.*[@#$%!]){1}.{8}$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(password);
-        return matcher.matches();
-    }
+	
 
     
 }
--- UserRegistrationJunit/src/test/java/com/UserRegJunit/AppTest.java
+++ UserRegistrationJunit/src/test/java/com/UserRegJunit/AppTest.java
@@ -2,28 +2,44 @@ package com.UserRegJunit;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-  
-	@Test
-    public void ValideEntryOfUser(){
-        UserRegistration ur=new UserRegistration();
-		assertEquals(true, ur.checkValidFirstName("Ash"));
-		assertEquals(true, ur.checkValidLastName("Shhhhh"));
-		assertEquals(true, ur.checkValidEmail("ash19@gmail.com"));
-		assertEquals(true, ur.checkValidMobileNumber("91 9999999999"));
-		assertEquals(true, ur.password("Avsoju9$"));
-		if(true) {
-			System.out.println("Happy");
+@RunWith(Parameterized.class)
+	public class AppTest 
+	{
+		String email;
+		boolean expection;
+		UserRegistration ur;
+		@Before
+	    public void init() {
+		ur=new UserRegistration();
 		}
+		public AppTest(String email,boolean expection) {
+			this.email=email;
+			this.expection=expection;
+			
+		}
+@Parameterized.Parameters
+	    public static Collection inputs(){
+			return Arrays.asList(new Object[][]{
+				{"abc@bridgelabz.co.in",true},{"abc@gmail.com.com",true},{"abc@yahoo.com",true},
+				{"abc@1.com",true},{"abc-100@yahoo.com",true},{"abc.100@yahoo.com",true},{"abc111@abc.com",true},
+				{"abc-100@abc.net",true},{"abc.100@abc.com.au",true},{"abc+100@gmail.com",true}
+				
+			}
+			);
+			}
+		@Test
+		public void checkFirstname() {
+		assertEquals(this.expection, ur.checkValidEmail(this.email));	
+		}
+
 	
 
 		
@@ -31,4 +47,4 @@ public class AppTest
 
 	
 	}
-}
+
